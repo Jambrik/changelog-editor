34d1cdac87c87f1941dce1ee81e4b05588d96bb4
Jambrik Róbert <rjambrik@mve.hu>
2017-12-18 23:12:46 +0100

    ChangeLogLoad works. http://localhost:3333/api/change-log-load/2/01.08.00
    It gives the id = 2 program 01.08.00 version change log.

--- server-modules/change-log.dao.js
+++ server-modules/change-log.dao.js
@@ -5,41 +5,44 @@ var configHelpers = require('./config-helpers');
 
 var fs = require('fs-extra');
 
-exports.changeLogLoad = function (param, res) {        
-    fs.readFile(param.programId + '.' + param.version + '.json', function (err, data) {
-        if (err) 
-            throw err;
-        res.json(JSON.parse(data));
-    });
+exports.changeLogLoad = function (param, res) {
+    let config = configDao.mainConfigLoad();
+    if (config) {
+        let program = configHelpers.getProgramById(config, param.programId);
+        if (program) {
+            let cl = fs.readJsonSync(program.path + "changelog" + '.' + param.version + '.json');
+            res.json(cl);            
+        }
+    }
 };
 
 exports.changeLogVersions = function (param, res) {
     //Get the program path:
-    console.log("in the changeLogVersion");    
-    console.log("programId", param.programId);                
-    if(param.programId){
-        let config = configDao.mainConfigLoad()
-        if(config){
+    console.log("in the changeLogVersion");
+    console.log("programId", param.programId);
+    if (param.programId) {
+        let config = configDao.mainConfigLoad();
+        if (config) {
             let program = configHelpers.getProgramById(config, param.programId);
-            if(program){
+            if (program) {
                 let path = program.path;
                 console.log("path", path);
-                fs.readdir(path, function(err, items) {                    
-                    if (err) 
+                fs.readdir(path, function (err, items) {
+                    if (err)
                         throw err;
                     let versions = [];
-                    for (var i=0; i<items.length; i++) {
-                        versions.push(items[i].substring(items[i].length-13, items[i].length-5));                        
+                    for (var i = 0; i < items.length; i++) {
+                        versions.push(items[i].substring(items[i].length - 13, items[i].length - 5));
                     }
-            
+
                     res.json(versions);
                 });
             }
         }
     }
-    
 
-    
+
+
 };
 
 
