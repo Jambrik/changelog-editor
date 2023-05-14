ae569d319308b6d77e8eb3c81c98069087ac0f7c
Jambrik Róbert <jambrik.robert@libra.hu>
2023-05-14 08:28:09 +0200

    Interface rename: removed the I prefix.

--- src/app/services/google-translate.service.ts
+++ src/app/services/google-translate.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Translate } from '../models/TranslateData';
+import { Translate } from '../models/Translate';
 
 @Injectable()
 export class GoogleTranslateService {
