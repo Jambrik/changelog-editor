086113d4c6f5fc8001ded740d3c29b3092d65fc5
Jambrik Róbert <jambrik.robert@libra.hu>
2023-05-21 17:10:03 +0200

    Proxy.config using. It existed but was not used.

--- server.js
+++ server.js
@@ -115,7 +115,6 @@ router.route('/changelog/versions/:program_id/')
 app.use('/rest', router);
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD');
   next();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  backEndUrl: 'http://localhost:3333/'
+  backEndUrl: ''
 };
